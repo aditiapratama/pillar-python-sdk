1afc0a2716523ab66eb7906580eba29b3775cd69
Francesco Siddi <francesco.siddi@gmail.com>
2015-10-28 20:34:36 +0100

    Organization import was missing for use in pillarsdk

--- pillarsdk/__init__.py
+++ pillarsdk/__init__.py
@@ -5,6 +5,7 @@ from .users import User
 from .files import File
 from .tokens import Token
 from .groups import Group
+from .organizations import Organization
 from .binary_files import binaryFile
 from .exceptions import ResourceNotFound, UnauthorizedAccess, MissingConfig
 from .config import __version__, __pypi_packagename__
