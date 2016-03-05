bec21e14e616ce6b06f58a4bed6dcd8dcb23ed77
Francesco Siddi <francesco.siddi@gmail.com>
2016-03-05 23:18:20 +0100

    Support for gravatar in User

--- pillarsdk/users.py
+++ pillarsdk/users.py
@@ -1,3 +1,5 @@
+import hashlib
+import urllib
 from .resource import List
 from .resource import Find
 from .resource import Create
@@ -10,3 +12,10 @@ class User(List, Find, Create, Post, Update, Delete):
     """User class wrapping the REST nodes endpoint
     """
     path = "users"
+
+    def gravatar(self, size=64):
+        """Generate Gravatar link on the fly using the email value"""
+        parameters = {'s':str(size), 'd':'mm'}
+        return "https://www.gravatar.com/avatar/" + \
+            hashlib.md5(self.email.lower()).hexdigest() + \
+            "?" + urllib.urlencode(parameters)
